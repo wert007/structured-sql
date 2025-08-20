b1b6eeb89e7097a59ae4873487c828e08fca1b11
wert007 <pablohamacher@web.de>
2025-08-20 17:41:44 +0200

    More debug

--- silo-derive/src/lib.rs
+++ silo-derive/src/lib.rs
@@ -1662,14 +1662,25 @@ fn create_row_type(
                 use silo::SqlTable;
                 let actual_column_name = row_name.map(|r| string_storage.store(&[r, "_", stringify!(#primary_key_field)])).unwrap_or(stringify!(#primary_key_field));
                 let #primary_key_field = <<#primary_key_type as silo::HasPartialRepresentation>::Partial>::try_from_row(string_storage, Some(actual_column_name), row, connection);
-                // dbg!(&#primary_key_field);
-                let #primary_key_field = #primary_key_field??;
-                let db = unsafe { silo::Database::from_connection(connection) }.ok()?;
-                let table = db.load::<#name>().ok()?;
+                let Some(Some(#primary_key_field)) = #primary_key_field else {
+                    eprintln!("Primary key field was none!!!");
+                    dbg!(&#primary_key_field, &row);
+                    return None;
+                };
+                let Some(db) = unsafe { silo::Database::from_connection(connection) }.ok() else {
+                    eprintln!("Database could not be loaded!");
+                    return None;
+                };
+                let Some(table) = db.load::<#name>().ok() else {
+                    eprintln!("Table could not be loaded!");
+                    return None;
+                };
 
-                let elements = table.filter(#filter_name::default().#has_primary_key_field(#primary_key_field.clone()));
-                // dbg!(&elements);
-                elements.ok()?.into_iter().next().map(|x| Some(x.into())).unwrap_or(Default::default())
+                let Some(elements) = table.filter(#filter_name::default().#has_primary_key_field(#primary_key_field.clone())).ok() else {
+                    eprintln!("Filtering the elements failed!");
+                    return None;
+                };
+                elements.into_iter().next().map(|x| Some(x.into())).unwrap_or(Default::default())
             }
         }
 
